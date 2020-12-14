b89f1f86b9daeeafa293dca58555cc930cbb3d50
joaoDossena <joaopdossena@gmail.com>
2020-12-14 12:30:34 +0000

    Added comments to add_tags testing file

--- src/test/add_tags_test.dart
+++ src/test/add_tags_test.dart
@@ -8,12 +8,16 @@ void main(){
  testWidgets('Adding tags as an admin', (tester) async {
     await tester.pumpWidget(MaterialApp(home: AddTags(),));
 
+    //Entering tag 'Artificial Intelligence' into tag field
     Finder tagField = find.byType(TextField);
     await tester.enterText(tagField, 'Artificial Intelligence');
 
+    //Tapping button to insert 'Artificial Intelligence' tag
     Finder button = find.byType(RaisedButton);
     await tester.tap(button);
     await tester.pump();
+
+    //Verifying that only the intended tag got inserted
     final artificialIntelligenceTag = find.text('Artificial Intelligence');
     expect(artificialIntelligenceTag, findsOneWidget);
     final dataScienceTag = find.text('Data Science');
