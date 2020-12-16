9d35bc9a3428110ee27faba53c12f72a59d7276c
joaoDossena <joaopdossena@gmail.com>
2020-12-16 16:13:53 +0000

    Tried adding MockAttendee to register test but to no avail

--- src/test/register_test.dart
+++ src/test/register_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hello/pages/register.dart';
+import 'package:hello/classes/person.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,11 +9,15 @@ import 'package:hello/authenticate/authentication.dart';
 import 'package:mockito/mockito.dart';
 
 class MockAuth extends Mock implements Authenticator {}
+class MockAttendee extends Mock implements Atendee {}
 
 
 void main(){
  testWidgets('Registering as a user', (tester) async {
     // MockAuth mockAuth = MockAuth();
+    // MockAttendee mockAttendee = MockAttendee();
+    // when(mockAuth.currentUser).thenReturn(mockAttendee);
+    // when(mockAttendee.id).thenReturn("MockID");
     // await tester.pumpWidget(MultiProvider(
     //   providers: [
     //     Provider<Authenticator>(
@@ -39,7 +44,7 @@ void main(){
     // await tester.enterText(locationField, 'Porto');
 
     // //Verifying that the email has been typed
-    // final email = find.text('test@gmail.com');
+    // final email = find.text('test2@gmail.com');
     // expect(email, findsOneWidget);
 
     // //Verifying that the password has been typed
