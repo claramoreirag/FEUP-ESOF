905946ef56d038b0302f5e849a14e0254875e038
claramoreirag <claramoreirag@users.noreply.github.com>
2020-12-21 17:46:07 +0000

    small changes

--- src/test/keywords_test.dart
+++ src/test/keywords_test.dart
@@ -13,27 +13,3 @@ void main() {
 
     await tester.pumpWidget(MaterialApp(
       home: chooseKeywords(mockAttendee, ['kw1', 'kw2', 'kw3']),
-    ));
-
-    var checkboxKeyword1 = find.byType(CheckboxListTile).first;
-
-    //Adding first interest
-    when(mockAttendee.interests).thenReturn(['kw1']);
-    await tester.tap(checkboxKeyword1);
-    await tester.pump();
-
-    //Verifying only the first interest was checked
-    verify(mockAttendee.addInterest('kw1')).called(1);
-    verifyNever(mockAttendee.addInterest('kw2'));
-    verifyNever(mockAttendee.addInterest('kw3'));
-
-    //Removing first interest
-    await tester.tap(checkboxKeyword1);
-    await tester.pump();
-
-    //Verifying only the first interest was removed
-    verify(mockAttendee.removeInterest('kw1')).called(1);
-    verifyNever(mockAttendee.removeInterest('kw2'));
-    verifyNever(mockAttendee.removeInterest('kw3'));
-  });
-}
